c0827aca6b19d3ae590b2a461660721705bcb2fe
A45hrith <72858755+A45hrith@users.noreply.github.com>
2025-02-10 13:09:30 +0000

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,11 +7,12 @@ const experienceData = [
     role: "Software Engineer II",
     duration: "Oct 2024 - Present",
     description: [
-      "Developed and optimized backend services using Java and Spring Boot, ensuring high scalability and performance.",
+      "Developed and optimised backend services using Java and Spring Boot, ensuring high scalability and performance.",
       "Designed and implemented RESTful APIs to streamline system integrations and enhance functionality.",
-      "Led efforts in CI/CD automation, reducing deployment time by 40% using Jenkins and Docker.",
+      "Led CI/CD pipeline automation resulting in reduced deployment time by 40% and eliminating manual errors through Jenkins and Docker.",
       "Utilised Kubernetes for container orchestration, improving application availability and resource efficiency.",
-      "Collaborated with front-end teams using React and Angular for seamless UX."
+      "Developed and implemented robust testing strategies using JUnit, PyTest, and Selenium, ensuring high reliability and reducing post-deployment defects by 30%.""
+
     ]
   },
   {
@@ -19,9 +20,12 @@ const experienceData = [
     role: "Full Stack Java Developer",
     duration: "Jul 2022 - Oct 2024",
     description: [
-      "Engineered Java-based backend solutions, ensuring security and scalability.",
-      "Automated CI/CD pipelines, improving deployment speed and system reliability.",
-      "Worked closely with DevOps teams to integrate cloud-based infrastructure on AWS."
+      "Engineered scalable and high-performance Java-based backend solutions for banking applications.",
+      "Upgraded DataStage 9.1 to 11.7, ensuring seamless data pipeline transitions and optimising ETL workflows for improved data processing efficiency.",
+      "Migrated data warehouses to Google Cloud Platform (GCP), leveraging SQL, XML, and JSON to enable scalable data storage, retrieval, and analytics.",
+      "Designed and implemented CI/CD pipelines using Jenkins, Docker, and Kubernetes, automating deployment workflows to eliminate manual interventions.",
+      "Improved deployment frequency by 30 – 40% and significantly reduced build errors, ensuring a more reliable and efficient release process.",
+      "Participated in the redesigning and development of wrap-around GUI screens using Node, React and TypeScript to replace and modernise legacy mainframe interfaces for increasing user experience, adoption, and interaction."
     ]
   },
   {
@@ -30,8 +34,11 @@ const experienceData = [
     duration: "Jul 2021 - Oct 2021",
     description: [
       "Built and enhanced Java-based backend services to support web applications.",
-      "Developed UI components and tested software using React and Jest.",
-      "Conducted API testing and debugging to improve system efficiency."
+      "Utilized UI/UX technologies including Figma, React, Node.js, HTML5, CSS, and JavaScript for both front-end and backend development.",
+      "Designed user interfaces with React, Node.js, and JavaScript to enhance engagement.",
+      "Collaborated with front-end teams using React and Angular to deliver seamless user experiences, ensuring backend services integrated smoothly with modern UI.",
+      "Conducted robust testing strategies using JUnit improving application reliability."
+
     ]
   },
   {
@@ -39,9 +46,11 @@ const experienceData = [
     role: "Developer Analyst",
     duration: "Nov 2020 - Jul 2021", // Corrected date range
     description: [
-      "Collaborated in Agile development cycles to improve software delivery efficiency.",
-      "Focused on C++ backend development, optimising database queries for performance.",
-      "Developed system monitoring tools for analysing application performance."
+      "Collaborated in Agile development cycles to improve software delivery efficiency, focusing on C++ backend development.",
+      "Developed and maintained scalable applications using C++ for backend logic, ensuring high performance and memory efficiency.",
+      "Worked on mobile development, contributing to Android/iOS applications using C++ (NDK) for performance-critical components.",
+      "Implemented RESTful APIs in C++ and integrated third-party services to enhance functionality, ensuring seamless data exchange.",
+      "Developed robust and scalable backend systems using C++, optimising algorithms for efficiency in real-time processing."
     ]
   }
 ];
